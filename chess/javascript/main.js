74f7d337926cdb28b6e841a7d8ca4d732f96c65b
Dev_Alberto <albertomeloteco@gmail.com>
2023-10-09 22:50:54 -0300

    09/10/2023

---
+++ chess/javascript/main.js
@@ -0,0 +1,12 @@
+var reset = window.document.querySelector('#reset');
+var config = window.document.querySelector('#set-up');
+
+//função para exibir a div
+ function hideDiv(e) {
+let setUP = window.document.querySelector('.' + e).style.display;
+if (setUP === 'none' || setUP === '') {
+    window.document.querySelector('.' + e).style.display = 'block';
+}else{
+    window.document.querySelector('.' + e).style.display = 'none';
+}
+}
