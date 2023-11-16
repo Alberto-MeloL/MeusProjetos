8a43abf7f47a428e9de048ea838b9b56b12ca229
Alberto-MeloL <albertomeloteco@gmail.com>
2023-11-16 11:09:29 -0300

    15/11/2023

---
+++ sales-box/src/app/app.routes.ts
@@ -0,0 +1,5 @@
+import { Routes } from '@angular/router';
+import { MenuConfiguracoesComponent } from './template/menu-configuracoes/menu-configuracoes.component';
+export const routes: Routes = [
+  {path: 'menu-configuracoes', component: MenuConfiguracoesComponent}
+];
---
+++ sales-box/src/app/template/menu-configuracoes/menu-configuracoes.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-menu-configuracoes',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './menu-configuracoes.component.html',
+  styleUrl: './menu-configuracoes.component.css'
+})
+export class MenuConfiguracoesComponent {
+
+}
